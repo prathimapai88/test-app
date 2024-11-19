ccae54ff18be2b3c4273b22e7287294fda859067
Prathima Shenoy <Prathimapai.88@gmail.com>
2024-11-19 16:16:29 +0000

    added filter

--- src/app/pages/pagination-table/pagination-table.component.ts
+++ src/app/pages/pagination-table/pagination-table.component.ts
@@ -6,8 +6,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { RouterModule } from '@angular/router';
 import { DxDataGridModule, DxFormModule, DxLoadIndicatorModule, DxPopoverModule } from 'devextreme-angular';
-import * as AspNetData from 'devextreme-aspnet-data-nojquery';
 import { PopoverComponentModule } from '../../shared/components/popover/popover.component';
+import CustomStore from 'devextreme/data/custom_store';
 
 
 @Component({
@@ -17,10 +17,46 @@ import { PopoverComponentModule } from '../../shared/components/popover/popover.
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class PaginationComponent {
-  dataSource = AspNetData.createStore({
+  dataSource = new CustomStore({
     key: 'Id',
-    loadUrl: 'https://js.devexpress.com/Demos/WidgetsGalleryDataService/api/Sales',
+    load: (loadOptions) =>{
+      const filters: any[] = [];
+      if (loadOptions.filter) {
+        this.processFilters(loadOptions.filter, filters);
+      }
+
+      // Prepare the request payload
+      const payload = {
+        filters: filters.map((filter) => ({
+          data_type: filter[0],
+          value: filter[2],
+        })),
+      };
+      return fetch('https://js.devexpress.com/Demos/WidgetsGalleryDataService/api/Sales', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(payload),
+      })
+        .then((res) => res.json())
+        .then((data) => ({
+          data: data.items,
+          totalCount: data.totalCount,
+        }));
+    }
+  
   });
+
+  processFilters(filterArray: any[], filters: any[]) {
+    if (Array.isArray(filterArray[0])) {
+      // Handle complex filter with logical operators
+      filterArray.forEach((filter) => this.processFilters(filter, filters));
+    } else if (filterArray.length === 3) {
+      // Add simple filter
+      filters.push(filterArray);
+    }
+  }
 }
 
 @NgModule({
